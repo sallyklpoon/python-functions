ee27ccf6bec3fd659d30472bbcfa520811c8127e
Sally Poon <65730547+sallyklpoon@users.noreply.github.com>
2021-01-31 03:16:25 -0800

    fixed mistake in romannumeral_upto_hundreds helper! program now works :) (yay! I can get some sleep now)

--- romannumeral.py
+++ romannumeral.py
@@ -11,35 +11,35 @@ def number_of_digits(integer):
     return len(str(integer))
 
 
-def integer_iterative(integer):
+def get_integer_iterative(integer):
     return [int(x) for x in str(integer)]
 
 
-def i_count(positive_int, int_numberplace):
-    roman_1 = roman_1s[int_numberplace]
+def count_ones(positive_int, place_value):
+    roman_1 = roman_1s[place_value]
     roman_number = ""
     for count in range(positive_int):
         roman_number = roman_number + roman_1
     return roman_number
 
 
-def romannumeral_loop(positive_int, int_numberplace):
-    roman_1 = roman_1s[int_numberplace]
-    roman_5 = roman_5s[int_numberplace]
+def romannumeral_1to9_loop(positive_int, place_value):
+    roman_1 = roman_1s[place_value]
+    roman_5 = roman_5s[place_value]
     if positive_int == 4:
         return roman_1 + roman_5
     elif positive_int >= 5:
         positive_int -= 5
-        return roman_5 + i_count(positive_int, int_numberplace)
+        return roman_5 + count_ones(positive_int, place_value)
     else:
-        return i_count(positive_int, int_numberplace)
+        return count_ones(positive_int, place_value)
 
 
 def romannumeral_upto_hundreds(positive_int):
     roman_number = ""
-    int_iterative = integer_iterative(positive_int)
-    for numbers_place in range(number_of_digits(positive_int) - 1):
-        roman_number += romannumeral_loop(int_iterative[numbers_place], numbers_place)
+    int_iterative = get_integer_iterative(positive_int)
+    for place_value in range(number_of_digits(positive_int)):
+        roman_number += romannumeral_1to9_loop(int_iterative[place_value], place_value)
     return roman_number
 
 
@@ -47,40 +47,43 @@ def romannumeral(positive_int):
     if positive_int == 10000:
         return roman_1s[3] * 10
     elif number_of_digits(positive_int) == 4:
-        int_iterative = integer_iterative(positive_int)
-        return i_count(int_iterative[0], 3) + romannumeral_upto_hundreds(positive_int - int_iterative[0]*1000) #something wrong here
+        thousands_digit = positive_int // 1000
+        return count_ones(thousands_digit, 3) + \
+            romannumeral_upto_hundreds(positive_int - thousands_digit * 1000)
     else:
         roman_number = ""
-        int_iterative = integer_iterative(positive_int)
+        int_iterative = get_integer_iterative(positive_int)
         for numbers_place in range(number_of_digits(positive_int)):
-            roman_number += romannumeral_loop(int_iterative[numbers_place], numbers_place)
+            roman_number += romannumeral_1to9_loop(int_iterative[numbers_place], numbers_place)
         return roman_number
 
 
 def main():
     print(number_of_digits(0))
     print(number_of_digits(1_000))
-    print(integer_iterative(123))
-    print(i_count(3, 0))
-    print(i_count(1, 1))
-    print(i_count(12, 2))
-    print(romannumeral_loop(1, 0))
-    print(romannumeral_loop(4, 0))
-    print(romannumeral_loop(5, 0))
-    print(romannumeral_loop(9, 0))
-    print(romannumeral_loop(1, 1))
-    print(romannumeral_loop(4, 1))
-    print(romannumeral_loop(5, 1))
-    print(romannumeral_loop(9, 1))
-    print(romannumeral_loop(1, 2))
-    print(romannumeral_loop(4, 2))
-    print(romannumeral_loop(5, 2))
-    print(romannumeral_loop(9, 2))
+    print(get_integer_iterative(123))
+    print(count_ones(3, 0))
+    print(count_ones(1, 1))
+    print(count_ones(12, 2))
+    print(romannumeral_1to9_loop(1, 0))
+    print(romannumeral_1to9_loop(4, 0))
+    print(romannumeral_1to9_loop(5, 0))
+    print(romannumeral_1to9_loop(9, 0))
+    print(romannumeral_1to9_loop(1, 1))
+    print(romannumeral_1to9_loop(4, 1))
+    print(romannumeral_1to9_loop(5, 1))
+    print(romannumeral_1to9_loop(9, 1))
+    print(romannumeral_1to9_loop(1, 2))
+    print(romannumeral_1to9_loop(4, 2))
+    print(romannumeral_1to9_loop(5, 2))
+    print(romannumeral_1to9_loop(9, 2))
     print(romannumeral(10000))
     print(romannumeral(4132))
-    print(romannumeral(555))
+    print(romannumeral(5555))
+    print(romannumeral(4444))
     print(romannumeral(354))
     print(romannumeral(789))
+    print(romannumeral(9999))
 
 
 if __name__ == "__main__":
