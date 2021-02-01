264217f5b678f16ba09e82860aa0676873a3e24e
Sally Poon <65730547+sallyklpoon@users.noreply.github.com>
2021-01-31 22:39:49 -0800

    completed lottery.py

---
+++ lottery.py
@@ -0,0 +1,25 @@
+"""This module contains a function that will create a list of 6 unique random numbers [1, 49].
+
+The sample() function from the random library has been imported."""
+
+from random import sample
+
+
+def lottery():
+    """Produce a list of 6 unique and random numbers in range [1,49].
+
+    :precondition: user calls lottery()
+    :postcondition: produce a list of 6 unique and random numbers within the range of [1,49]
+    :return: list of 6 unique and random numbers
+    """
+    return sample(range(1, 49), k=6)
+
+
+def main():
+    print(lottery())
+    print(lottery())
+    print(lottery())
+
+
+if __name__ == '__main__':
+    main()
