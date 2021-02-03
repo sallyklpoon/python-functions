e4c9410c76b2101fd994139493fd166439f8fd9f
Sally Poon <65730547+sallyklpoon@users.noreply.github.com>
2021-02-02 23:22:45 -0800

    found a bug when testing, fixed!!

--- eratosthenes.py
+++ eratosthenes.py
@@ -1,7 +1,7 @@
 """This module contains a function that uses Eratosthenes method
 of finding a list of prime numbers given a range from [0, n],
 """
-from math import sqrt, floor
+from math import sqrt, ceil
 
 
 def eratosthenes(upper_bound):
@@ -13,7 +13,7 @@ def eratosthenes(upper_bound):
     :return: a list of prime numbers up to the upper_bound range
     """
     number_list = list(range(2, upper_bound + 1))
-    evaluating_numbers = list(range(2, floor(sqrt(upper_bound))))
+    evaluating_numbers = list(range(2, ceil(sqrt(upper_bound))))
     for evaluating_number in evaluating_numbers:
         for number in number_list:
             if number != evaluating_number and number % evaluating_number == 0:
@@ -23,11 +23,14 @@ def eratosthenes(upper_bound):
 
 def main():
     number_list = [0, 2, 3, 4]
+
     number_list.remove(3)
     input_integer = 100
     print(eratosthenes(input_integer))
     print(eratosthenes(16))
     print(eratosthenes(19))
+    primes_below_30 = eratosthenes(30)
+    print(primes_below_30)
 
 
 if __name__ == '__main__':
