d7cf0c98dcb65beb1a08d689e030d6f385fb963e
Sally Poon <65730547+sallyklpoon@users.noreply.github.com>
2021-02-06 00:19:07 -0800

    Modified main() and edited docstring + module comments.

--- lottery.py
+++ lottery.py
@@ -1,6 +1,13 @@
-"""This module contains a function that will create a list of 6 unique random numbers [1, 49].
+"""
+Sally Poon ; A01232177
+Date Completed: 02-05-2021
 
-The sample() function from the random library has been imported."""
+This module contains a function that will create a list of 6 unique random numbers [1, 49].
+
+The sample() function from the random library has been imported.
+
+For unit test, run test_lottery.py
+"""
 
 from random import sample
 
@@ -10,15 +17,13 @@ def lottery():
 
     :precondition: user calls lottery()
     :postcondition: produce a list of 6 unique and random numbers within the range of [1,49]
-    :return: list of 6 unique and random numbers
+    :return: list of 6 unique and random numbers in the range [1, 49]
     """
     lottery_numbers = sample(range(1, 49), k=6)
     return sorted(lottery_numbers)
 
 
 def main():
-    print(lottery())
-    print(lottery())
     print(lottery())
 
 
