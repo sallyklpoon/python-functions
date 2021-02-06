5e446be64b8b035f830c6ea6fff584ac93ebcf00
Sally Poon <65730547+sallyklpoon@users.noreply.github.com>
2021-02-05 18:56:26 -0800

    finally a working dijkstra function! Changed to count + using .split string function

--- dijkstra.py
+++ dijkstra.py
@@ -1,29 +1,23 @@
-
-
 def dijkstra(colours_given):
-    for colour in colours_given:
-        if colour == "red":
-            colour_adjust = colours_given.pop(colours_given.index(colour))
-            colours_given.insert(0, colour_adjust)
-        elif colour == "blue":
-            colour_adjust = colours_given.pop(colours_given.index(colour))
-            colours_given.append(colour_adjust)
-        else:
-            colour_adjust = colours_given.pop(colours_given.index(colour))
-            white_position = colours_given.count("red")
-            colours_given.insert(white_position, colour_adjust)
+    red_tally = colours_given.count("red")
+    white_tally = colours_given.count("white")
+    blue_tally = colours_given.count("blue")
+    written_tally = "red." * red_tally + "white." * white_tally + "blue." * blue_tally
+    dutch_colours = written_tally[:-1].split(".")
+    colours_given.clear()
+    for item in dutch_colours:
+        colours_given.append(item)
 
 
 def main():
-    tests_lst = ["blue", "blue", "blue", "blue", "white", "blue", "blue", "red", "red", "red", "red", "red"]
+    """Execute the program."""
+    tests_lst = ["white", "blue", "blue", "red", "white", "red", "white"]
+    # Executes to ['red', 'white', 'white', 'red', 'blue', 'blue']
     dijkstra(tests_lst)
     print(tests_lst)
+    # string = "red." * 4 + "white." * 3 + "blue." * 3
+    # print(string.split("."))
 
-    # colour_adjust = tests_lst.pop(-1)
-    # tests_lst.insert(len(tests_lst) // 2, colour_adjust)
-    # print(len(tests_lst) // 2)
-    # print(tests_lst)
-
-
+# Execute only if file run as a program
 if __name__ == '__main__':
     main()
