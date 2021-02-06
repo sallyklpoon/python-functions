7792659c88531e5ab9abe8031e2dd0dab50ec27c
Sally Poon <65730547+sallyklpoon@users.noreply.github.com>
2021-02-06 15:33:21 -0800

    doctests complete, small edits to docstrings.

--- completed_functions/moneychanger.py
+++ completed_functions/moneychanger.py
@@ -1,23 +1,53 @@
-"""Module includes a function called moneychanger and roundoff_pennies.
+"""
+Sally Poon A01232177
+Date Completed: 02-06-2021
 
-round_off pennies will round given dollar amount to closes nickel or dime.
-
-moneychanger will produce a list of change needed to cover a total dollar amount.
+Module includes a function called moneychanger and roundoff_pennies.
+    round_off pennies will round given dollar amount to closes nickel or dime.
+    moneychanger will produce a list of change needed to cover a total dollar amount.
 
 This module requires the imported function trunc from Python's math library.
 
 Canadian_cash defines as a global list of cash amounts available in canadian currency.
 """
+import doctest
 from math import trunc
 
 
 def roundoff_pennies(total_dollars):
     """Round out pennies from given dollar amount.
 
-    :param total_dollars: a positive floating point number with 2 decimals
+    :param total_dollars: a float with up to 2 decimal points
     :precondition: total_dollars value is a floating point >= 0.00 with 2 decimals
-    :postcondition: return the total_dollars value rounded off without pennies
+    :postcondition: return the total_dollars value rounded off without pennies (to the closest multiple of 5)
     :return: a floating point of dollar amount rounded to closest nickel, dime, or dollar to 2 decimal places
+
+    >>> roundoff_pennies(0.00)
+    0.0
+    >>> roundoff_pennies(0.01)
+    0.0
+    >>> roundoff_pennies(0.02)
+    0.0
+    >>> roundoff_pennies(0.03)
+    0.05
+    >>> roundoff_pennies(0.04)
+    0.05
+    >>> roundoff_pennies(0.05)
+    0.05
+    >>> roundoff_pennies(0.06)
+    0.05
+    >>> roundoff_pennies(0.07)
+    0.05
+    >>> roundoff_pennies(0.08)
+    0.1
+    >>> roundoff_pennies(0.09)
+    0.1
+    >>> roundoff_pennies(0.10)
+    0.1
+    >>> roundoff_pennies(999.99)
+    1000.0
+    >>> roundoff_pennies(1023.29)
+    1023.3
     """
     hundredths = int(str(total_dollars)[-1])
     if str(total_dollars)[-2] == ".":
@@ -39,36 +69,58 @@ def roundoff_pennies(total_dollars):
 def moneychanger(total_dollars):
     """Produce list of change in Canadian currency to cover total_dollars amount.
 
-    :param total_dollars: a positive floating point number with 2 decimals
-    :precondition: total_dollars value is a floating point >= 0.00 with 2 decimals
+    :param total_dollars: a positive floating point number up to 2 decimals places
+    :precondition: total_dollars value is a floating point >= 0.00 with up to 2 decimal places
     :postcondition: return accurate breakdown of Canadian currency to cover the total_dollars amount
                     in a list from largest currency amount to smallest.
     :return: a list with the number of each currency required following this format...
             [$100-bill, $50-bill, $20-bill, $10-bill, $5-bill, $2, $1, 0.25 cents, 0.10 cents, 0.05 cents]
+
+    Test that conversions work:
+    >>> moneychanger(0.05)
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    >>> moneychanger(0.10)
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+    >>> moneychanger(0.25)
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+    >>> moneychanger(1.00)
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+    >>> moneychanger(2.00)
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+    >>> moneychanger(5.00)
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+    >>> moneychanger(10.00)
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+    >>> moneychanger(20.00)
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+    >>> moneychanger(50.00)
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    >>> moneychanger(100.00)
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    Test other cases to combine
+    >>> moneychanger(0.00)
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    >>> moneychanger(587.34)
+    [5, 1, 1, 1, 1, 1, 0, 1, 1, 0]
+    >>> moneychanger(999.99)
+    [10, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     """
     canadian_change = [100, 50, 20, 10, 5, 2, 1, 0.25, 0.10, 0.05]
     output_change = []
     total_dollars = roundoff_pennies(total_dollars)
     for change_value in canadian_change:
         output_change.append(trunc(total_dollars // change_value))
-        total_dollars = round((((total_dollars / change_value) - (total_dollars // change_value)) * change_value), 2)
+        total_dollars = round((((total_dollars / change_value)
+                                - (total_dollars // change_value)) * change_value), 2)
     return output_change
 
 
 def main():
-    total_dollars = 999.97
-    print(roundoff_pennies(total_dollars))
-    print(moneychanger(total_dollars))
-
-    # print(roundoff_pennies(999.99))
-    # print(int(str(total_dollars)[-1]))
-    # print(roundoff_pennies(999.80))
-    # print(roundoff_pennies(999.93))
-    # print(roundoff_pennies(999.95))
-    # print(10 % 5)
-    # print(999.97 + round((0.05 - 7 * 10 ** -2), 2))
-    pass
+    """Execute the program."""
+    doctest.testmod(verbose=True)
 
 
 if __name__ == '__main__':
+    # Run main() if module is being run as program
     main()
