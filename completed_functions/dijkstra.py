965d868f5cabfb897d969444b2ead47ddbdc297d
Sally Poon <65730547+sallyklpoon@users.noreply.github.com>
2021-02-06 15:57:22 -0800

    doctests complete, added docstrings. Simplified function to get rid of unnecessary variables colour_tally

--- completed_functions/dijkstra.py
+++ completed_functions/dijkstra.py
@@ -1,23 +1,75 @@
+"""
+Sally Poon A01232177
+Date Completed: 02-06-2021
+
+This module contains a function to solve the dijkstra problem without any
+return or print value -- that is, the function will only modify the original list.
+"""
+import doctest
+
+
 def dijkstra(colours_given):
-    red_tally = colours_given.count("red")
-    white_tally = colours_given.count("white")
-    blue_tally = colours_given.count("blue")
-    written_tally = "red." * red_tally + "white." * white_tally + "blue." * blue_tally
-    dutch_colours = written_tally[:-1].split(".")
+    """Modify an original list of "red", "white", and "blue" by sorting elements in the order of the National
+    Dutch Flag colours.
+
+    A solution to the Dijkstra problem.
+
+    :param colours_given: a list of strings
+    :precondition: given list is a collection of string elements "red", "white", or "blue" only
+    :postcondition: modify the list accurately to have all elements in order of "red", "white", then "blue"
+    :return: nothing, modifies original list
+
+    >>> dutch = ["red", "white", "blue"]
+    >>> dijkstra(dutch)
+    >>> print(dutch)
+    ['red', 'white', 'blue']
+    >>> dutch = ["red", "blue", "white"]
+    >>> dijkstra(dutch)
+    >>> print(dutch)
+    ['red', 'white', 'blue']
+    >>> dutch = ["blue", "white", "red"]
+    >>> dijkstra(dutch)
+    >>> print(dutch)
+    ['red', 'white', 'blue']
+    >>> dutch = ["white", "red", "blue"]
+    >>> dijkstra(dutch)
+    >>> print(dutch)
+    ['red', 'white', 'blue']
+    >>> dutch = ["blue", "blue", "red", "blue", "red", "red"]
+    >>> dijkstra(dutch)
+    >>> print(dutch)
+    ['red', 'red', 'red', 'blue', 'blue', 'blue']
+    >>> dutch = ["red", "white", "white", "red", "red", "white"]
+    >>> dijkstra(dutch)
+    >>> print(dutch)
+    ['red', 'red', 'red', 'white', 'white', 'white']
+    >>> dutch = ["white", "blue", "blue", "blue", "white", "blue"]
+    >>> dijkstra(dutch)
+    >>> print(dutch)
+    ['white', 'white', 'blue', 'blue', 'blue', 'blue']
+    >>> dutch = ["white", "red", "red", "white", "red", "red"]
+    >>> dijkstra(dutch)
+    >>> print(dutch)
+    ['red', 'red', 'red', 'red', 'white', 'white']
+    >>> dutch = ["white", "blue", "blue", "red", "white", "red", "white"]
+    >>> dijkstra(dutch)
+    >>> print(dutch)
+    ['red', 'red', 'white', 'white', 'white', 'blue', 'blue']
+    """
+    tally = "red." * colours_given.count("red") \
+            + "white." * colours_given.count("white") \
+            + "blue." * colours_given.count("blue")
+    dutch_colours = tally[:-1].split(".")
     colours_given.clear()
-    for item in dutch_colours:
-        colours_given.append(item)
+    for element in dutch_colours:
+        colours_given.append(element)
 
 
 def main():
     """Execute the program."""
-    tests_lst = ["white", "blue", "blue", "red", "white", "red", "white"]
-    # Executes to ['red', 'white', 'white', 'red', 'blue', 'blue']
-    dijkstra(tests_lst)
-    print(tests_lst)
-    # string = "red." * 4 + "white." * 3 + "blue." * 3
-    # print(string.split("."))
+    doctest.testmod(verbose=True)
 
-# Execute only if file run as a program
+
 if __name__ == '__main__':
+    # Run main() if module is being run as program
     main()
