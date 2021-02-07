d7eea743b282499802c727a6a1e30e314879f9b4
Sally Poon <65730547+sallyklpoon@users.noreply.github.com>
2021-02-07 12:51:28 -0800

    imported lottery() from lottery.py

--- completed_functions/test_lottery.py
+++ completed_functions/test_lottery.py
@@ -11,7 +11,7 @@ in addition to lottery from lottery.py.
 from unittest import TestCase
 from unittest.mock import patch
 
-import lottery as lottery
+from lottery import lottery
 
 
 class TestLottery(TestCase):
@@ -19,26 +19,26 @@ class TestLottery(TestCase):
     def test_lottery_correct_length(self):
         # Test that there are 6 numbers
         expected = 6
-        actual = len(lottery.lottery())
+        actual = len(lottery())
         self.assertEqual(expected, actual)
 
     def test_lottery_element_within_range(self):
         # Test that each element in lottery() list is [1, 49]
         expected = range(1, 50)
-        actual_lottery = lottery.lottery()
+        actual_lottery = lottery()
         for number in actual_lottery:
             actual_number = number
             self.assertIn(actual_number, expected)
 
     def test_lottery_sorted(self):
         # Test that lottery() list is sorted from least to greatest
-        actual = lottery.lottery()
+        actual = lottery()
         expected = sorted(actual)
         self.assertTrue(expected, actual)
 
     def test_lottery_unique(self):
         # Test that each number in lottery() is unique
-        actual_lottery = lottery.lottery()
+        actual_lottery = lottery()
         for number in actual_lottery:
             expected_distinct_count = 1
             actual_count = actual_lottery.count(number)
@@ -47,6 +47,6 @@ class TestLottery(TestCase):
     @patch('lottery.sample', return_value=[49, 34, 28, 11, 9, 1])
     def test_lottery_single_lottery(self, mock_randomsample):
         # Patch random.sample, test logic of function
-        actual = lottery.lottery()
+        actual = lottery()
         self.assertEqual(actual, [1, 9, 11, 28, 34, 49])
 
