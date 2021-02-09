f6380b70dc9043cfbe6f32294b9584d62fd315d4
Sally Poon <65730547+sallyklpoon@users.noreply.github.com>
2021-02-08 21:59:42 -0800

    edited doctests

--- completed_functions/compoundtinterest.py
+++ completed_functions/compoundtinterest.py
@@ -14,8 +14,8 @@ def compoundinterest(principal, interest_rate, compoundtimes_peryear, years_to_g
 
     :param principal: any number
     :param interest_rate: a positive or negative float
-    :param compoundtimes_peryear: an integer > 0
-    :param years_to_grow: a float > 0
+    :param compoundtimes_peryear: an integer
+    :param years_to_grow: a float
     :precondition: principal must be a number that is positive or negative up to 2 decimal places
     :precondition: interest_rate must be a positive or negative number
     :precondition: compoundtimes_peryear must be an integer greater than 0
@@ -23,24 +23,19 @@ def compoundinterest(principal, interest_rate, compoundtimes_peryear, years_to_g
     :postcondition: the accurate raw number of money accrued over the years will be calculated
     :return: amount of money accrued in account as a float, raw number
 
-    >>> compoundinterest(0, 0, 1, 0.5)
+    >>> compoundinterest(0, 0, 1, 0.1)
     0.0
-    >>> compoundinterest(-100.50, 0.25, 1, 0.012)
-    -100.76947174716699
-    >>> compoundinterest(-10, -0.011, 1, 0.012)
+    >>> compoundinterest(1000, 0.0, 2, 2) # 0 interest rate
+    1000.0
+    >>> compoundinterest(-10, -0.011, 1, 0.012) # principal and interest rate negative
     -9.998672774401053
-    >>> compoundinterest(324_099, -1.5, 4, 1)
+    >>> compoundinterest(324_099, -1.5, 4, 1) # negative interest rate
     49453.582763671875
-    >>> compoundinterest(100.50, 1.678, 4, 3.25)
-    9548.303632583229
-    >>> compoundinterest(-37, 80, 1, 1)
-    -2997.0
-    >>> compoundinterest(100, 1.1, 356, 1)
-    299.9075439755226
-    >>> compoundinterest(1, .01, 0.5, 300)
-    19.4996027667626
+    >>> compoundinterest(1000, 0.1, 100, 100) # principal and interest rate positive
+    21916681.339054313
     """
-    return principal * (1 + interest_rate / compoundtimes_peryear) ** (compoundtimes_peryear * years_to_grow)
+    accrued_money = principal * (1 + interest_rate / compoundtimes_peryear) ** (compoundtimes_peryear * years_to_grow)
+    return accrued_money
 
 
 def main():
